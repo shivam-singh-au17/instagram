65aef3a7e1a82e9ffd5221818296307c5b2e659b
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-01 17:00:11 +0530

    adding some file

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/blogsdata"));
-const dbo = require("./db/conn");
+
+app.use(require("./src/controller/user.controller"));
+const dbo = require("./src/config/db");
 
 app.get("/", async (req, res) => {
   try {
-    return res.redirect("/blogsdata/");
+    return res.redirect("/user/");
   } catch (err) {
     return res.status(400).send(err.message);
   }
