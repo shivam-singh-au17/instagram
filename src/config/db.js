b24c9bb2dd1e7f395c96ef0fe381d0065bac786d
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-01 09:54:03 +0530

    some changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,29 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 
 const db = process.env.DB_URL
 
-let connect = {
-    dbUrl: db
-};
+const connect = () => {
+
+    return mongoose.connect(db, {
+        keepAlive: 1,
+        connectTimeoutMS: 30000,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err) => {
+
+        if (err) {
+            console.log("ERROR", err);
+        }
+
+    });
+}
+
 
 module.exports = connect;
 
