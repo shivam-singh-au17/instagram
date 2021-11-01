d3dd68a903b3a6cf885252220b4bd233f0a12e6e
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-01 11:22:21 +0530

    some changes

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
-
-const postcontroller = require("./src/controllers/posts.controlers");
-
-const connect = require("./src/config/db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 const port = process.env.PORT || 3030
 
-app.use("/post", postcontroller);
+const connect = require("./src/config/db");
+
+const postcontroller = require("./src/controllers/posts.controlers");
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", postcontroller);
+
+
+app.get("/", async (req, res) => {
+  try {
+    return res.redirect("/post");
+  } catch (err) {
+    return res.status(400).send(err.message);
+  }
+});
+
 
 app.listen(port, async () => {
   // RUN THE CONNECT FUNCTION
