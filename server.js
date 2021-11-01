77f5b5cf52c228beadcdaf028ab3fee253536f69
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-01 17:47:48 +0530

    file adding

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3030
 
@@ -11,8 +10,8 @@ const connect = require("./src/config/db");
 const postcontroller = require("./src/controllers/posts.controlers");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 app.use(postcontroller);
 
